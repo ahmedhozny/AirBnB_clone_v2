1067e6a363ded54c054bf120df4aeb478cfbe055
Ahmed Hosny <ahmhosny2002@gmail.com>
2024-02-05 04:08:48 +0300

    Added db_storage.py (WIP)

---
+++ models/engine/db_storage.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" new class for db management """
+
+from os import getenv
+
+from sqlalchemy import create_engine
+
+
+class DBStorage:
+    """ create tables in environmental"""
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        user = getenv("HBNB_MYSQL_USER")
+        passwd = getenv("HBNB_MYSQL_PWD")
+        db = getenv("HBNB_MYSQL_DB")
+        host = getenv("HBNB_MYSQL_HOST")
+        env = getenv("HBNB_ENV")
+
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(user, passwd, host, db),
+                                      pool_pre_ping=True)
